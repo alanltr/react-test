cb9cdb21308e02343af3ae6d9cad9b81dbd2fd6c
alanltr <letrocqueralan@gmail.com>
2021-04-09 14:42:50 +0200

    composant Cards & Card statiques OK

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,5 +1,8 @@
 // == Import npm
 import React from 'react';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ThumbDownIcon from '@material-ui/icons/ThumbDown';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 // == Import
 import './cards.scss';
@@ -7,7 +10,20 @@ import './cards.scss';
 // == Composant
 const Card = () => (
   <div className="card-component">
-    <h1>Composant : Card</h1>
+    <h2 className="card-title">Les Indestructibles 2</h2>
+    <div className="card-category">
+      Catégorie
+    </div>
+    <div className="card-thumbup">
+      <ThumbUpIcon />
+    </div>
+    <div className="card-thumbup">
+      <ThumbDownIcon />
+    </div>
+    <DeleteForeverIcon />
+    <div className="card-like-dislike-ratio">
+      <div />
+    </div>
   </div>
 );
 
