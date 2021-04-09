41db07ceab1ef6fc03147408937727b5ce23e2e8
alanltr <letrocqueralan@gmail.com>
2021-04-09 19:36:46 +0200

    ajout de la barre des likes

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -11,25 +11,28 @@ const Card = ({
   category,
   likes,
   dislikes,
-}) => (
-  <div className="card-component">
-    <h2 className="card-title">{title}</h2>
-    <div className="card-category">
-      {category}
-    </div>
-    <div className="card-thumbup">
-      <ThumbUpIcon />
-    </div>
-    <div className="card-thumbdown">
-      <ThumbDownIcon />
-    </div>
-    <DeleteForeverIcon />
-    <div className="card-like-dislike-ratio">
-      <div />
-    </div>
-  </div>
-);
+}) => {
+  const likePercentage = `${Math.round(((likes / (likes + dislikes)) * 100))}%`;
 
+  return (
+    <div className="card-component">
+      <h2 className="card-title">{title}</h2>
+      <div className="card-category">
+        {category}
+      </div>
+      <div className="card-thumbup">
+        <ThumbUpIcon />
+      </div>
+      <div className="card-thumbdown">
+        <ThumbDownIcon />
+      </div>
+      <DeleteForeverIcon />
+      <div className="card-like-dislike-ratio">
+        <div style={{ width: `${likePercentage}` }} />
+      </div>
+    </div>
+  );
+}
 Card.propTypes = {
   title: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
